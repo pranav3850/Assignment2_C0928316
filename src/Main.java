0688e884f3ac2162c3962e0de759708e8f596076
pranav3850 <pranavgoswami38@gmail.com>
2025-02-19 01:23:15 -0500

    Commit Part2 for assignment

--- src/Main.java
+++ src/Main.java
@@ -15,17 +15,12 @@ public class Main {
             List<Person> people = objectMapper.readValue(new File("D:/Lambton/sem-3/Java/Assignment2_C0928316/src/person.json"), new TypeReference<List<Person>>() {});
             List<BlogPost> blogPosts = objectMapper.readValue(new File("D:/Lambton/sem-3/Java/Assignment2_C0928316/src/blogPosts.json"), new TypeReference<List<BlogPost>>() {});
 
-            // Create Blog instance
             Blog blog = new Blog(blogPosts, people);
 
-            // Get blog post IDs by author age
             List<String> postsByAge = blog.getPostsByAuthorAge(28);
             System.out.println("Blog Posts by Authors aged 28: " + postsByAge);
-
-            // Print total blog posts and unique contributors
             System.out.println("Total blog posts: " + blogPosts.size());
             System.out.println("Total unique authors: " + people.size());
-
         } catch (IOException e) {
             System.err.println("Error reading JSON files: " + e.getMessage());
         }
